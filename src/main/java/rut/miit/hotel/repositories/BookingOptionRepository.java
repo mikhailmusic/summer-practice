aae862ec717dec0233bb8e335ef1ce2118960881
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2024-07-14 22:36:36 +0300

    connect ModelMapper

--- src/main/java/rut/miit/hotel/repositories/BookingOptionRepository.java
+++
@@ -1,9 +0,0 @@
-package rut.miit.hotel.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import rut.miit.hotel.entities.BookingOption;
-
-@Repository
-public interface BookingOptionRepository extends JpaRepository<BookingOption, Integer> {
-}
