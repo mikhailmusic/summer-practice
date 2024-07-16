a235a881c585f3e25ca47c2ee108f5d3849296d9
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2024-07-16 14:24:23 +0300

    fix

--- src/main/java/rut/miit/hotel/service/impl/HotelDomainServiceImpl.java
+++ src/main/java/rut/miit/hotel/service/impl/HotelDomainServiceImpl.java
@@ -8,7 +8,6 @@ import rut.miit.hotel.dto.response.HotelResponseDto;
 import rut.miit.hotel.service.BookingDomainService;
 import rut.miit.hotel.service.HotelDomainService;
 import rut.miit.hotel.repositories.HotelRepository;
-import rut.miit.hotel.repositories.RoomRepository;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,13 +17,11 @@ import java.util.List;
 public class HotelDomainServiceImpl implements HotelDomainService {
 
     private final HotelRepository hotelRepository;
-    private final RoomRepository roomRepository;
     private final BookingDomainService BookingDomainService;
     private final ModelMapper modelMapper;
 
-    public HotelDomainServiceImpl(HotelRepository hotelRepository, RoomRepository roomRepository, BookingDomainServiceImpl bookingService, ModelMapper modelMapper) {
+    public HotelDomainServiceImpl(HotelRepository hotelRepository, BookingDomainServiceImpl bookingService, ModelMapper modelMapper) {
         this.hotelRepository = hotelRepository;
-        this.roomRepository = roomRepository;
         this.BookingDomainService = bookingService;
         this.modelMapper = modelMapper;
     }
@@ -35,22 +32,17 @@ public class HotelDomainServiceImpl implements HotelDomainService {
 
         List<Hotel> hotels = hotelRepository.findByCountryAndCityAndRating(country, city, rating);
         List<Hotel> availableHotels = new ArrayList<>();
-
-        List<Room> rooms;
-        for (int i = 0; i < hotels.size(); i++) {
-            rooms = roomRepository.findRoomsByHotelId(hotels.get(i).getId());
-
-            rooms = rooms.stream()
+        for (Hotel hotel : hotels) {
+            List<Room> rooms = hotel.getRooms().stream()
                     .filter(room -> BookingDomainService.isRoomAvailable(room, startDate, endDate))
                     .filter(room -> capacity == null || room.getCapacity() >= capacity)
                     .filter(room -> maxPrice == null || room.getPricePerNight().compareTo(maxPrice) <= 0)
                     .toList();
 
             if (!rooms.isEmpty()) {
-                hotels.get(i).setRooms(rooms);
-                availableHotels.add(hotels.get(i));
+                hotel.setRooms(rooms);
+                availableHotels.add(hotel);
             }
-
         }
 
         return availableHotels.stream().map(e -> modelMapper.map(e, HotelResponseDto.class)).toList();
