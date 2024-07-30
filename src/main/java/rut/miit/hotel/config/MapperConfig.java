c518950d327bba7eb4b5309056044a70d2e81c8f
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2024-07-30 11:01:11 +0300

    change config

--- src/main/java/rut/miit/hotel/config/MapperConfig.java
+++ src/main/java/rut/miit/hotel/config/MapperConfig.java
@@ -3,17 +3,12 @@ package rut.miit.hotel.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import rut.miit.hotel.domain.Booking;
-import rut.miit.hotel.dto.response.BookingResponseDto;
 
 @Configuration
 public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.typeMap(Booking.class, BookingResponseDto.class).addMapping(Booking::getPayments, BookingResponseDto::setPayments);
-
-        return modelMapper;
+        return new ModelMapper();
     }
 }
